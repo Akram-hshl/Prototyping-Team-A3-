46b94e9237a50d8c455a115f16ff36060cfe18c2
Omar Latif <37932353+omarabdellatif99@users.noreply.github.com>
2025-05-17 17:36:42 +0200

    config header file

---
+++ V4_OrganizeD_CodeBase/config.h
@@ -0,0 +1,23 @@
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Motor pins
+#define enA 10
+#define in1 9
+#define in2 8
+#define in3 7
+#define in4 6
+#define enB 5
+
+// Sensors and Servo
+#define L_S A0
+#define R_S A1
+#define echo A2
+#define trigger A3
+#define servo A5
+
+// Threshold
+#define OBSTACLE_THRESHOLD 10
+
+#endif
